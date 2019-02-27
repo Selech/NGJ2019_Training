67a19f65d8c452ae7d7c75c787b82e68fb5bb67e
Peter Scheel <ps@dalux.com>
2019-02-27 18:46:43 +0100

    Fixed camera

--- NGJ2019_Training/Assets/Scripts/CameraController.cs
+++ NGJ2019_Training/Assets/Scripts/CameraController.cs
@@ -4,49 +4,49 @@ using UnityEngine;
 
 public class CameraController : GameControllerCommunicator
 {
-    public GameController Controller;
-    public float HightFloatHighestPoint = 5;
+	public GameController Controller;
+	public float HightFloatHighestPoint = 5;
 
-    // Screen shake effect
-    private Transform transform;
-    private float shakeDuration = 0f;
-    private float shakeMagnitude = 0.15f;
-    private float dampingSpeed = 2.0f;
-    Vector3 initialPosition;
+	// Screen shake effect
+	private Transform transform;
+	private float shakeDuration = 0f;
+	private float shakeMagnitude = 0.15f;
+	private float dampingSpeed = 2.0f;
+	Vector3 initialPosition;
 
-    void Awake()
-    {
-        Controller = GameObject.FindObjectOfType<GameController>();
+	void Awake()
+	{
+		Controller = GameObject.FindObjectOfType<GameController>();
 
-        transform = GetComponent(typeof(Transform)) as Transform;
-        initialPosition = transform.localPosition;
-    }
+		transform = GetComponent(typeof(Transform)) as Transform;
+		initialPosition = transform.localPosition;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (shakeDuration > 0)
-        {
-            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
-            shakeDuration -= Time.deltaTime * dampingSpeed;
-        }
-        else
-        {
-            shakeDuration = 0f;
-            transform.localPosition = initialPosition;
+	// Update is called once per frame
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			transform.localPosition = this.transform.position + Random.insideUnitSphere * shakeMagnitude;
+			shakeDuration -= Time.deltaTime * dampingSpeed;
+		}
+		else
+		{
+			this.transform.position = new Vector3(initialPosition.x, this.transform.position.y, initialPosition.z);
+			shakeDuration = 0f;
 
-            var position = this.transform.position;
-            position = new Vector3(position.x, Controller.CurrentHighPoint + HightFloatHighestPoint, position.z);
-            if (position.y < 10)
-            {
-                return;
-            }
-            this.transform.position = Vector3.Slerp(this.transform.position, position, 0.15f * Time.deltaTime);
-        }
-    }
+			var position = this.transform.position;
+			position = new Vector3(initialPosition.x, Controller.CurrentHighPoint + HightFloatHighestPoint, initialPosition.z);
+			if (position.y < 10)
+			{
+				return;
+			}
+			this.transform.position = Vector3.Slerp(this.transform.position, position, 0.15f * Time.deltaTime);
+		}
+	}
 
-    public void ShakeCamera()
-    {
-        shakeDuration = 0.10f;
-    }
+	public void ShakeCamera()
+	{
+		shakeDuration = 0.10f;
+	}
 }
