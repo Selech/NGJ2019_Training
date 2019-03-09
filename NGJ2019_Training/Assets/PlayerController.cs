a1b116961d49c5187ff061edb37780edb3950bd1
selech <selech@live.dk>
2019-03-09 14:40:42 +0100

    Reverted and changed a bunch of stuff to improve physics

--- NGJ2019_Training/Assets/PlayerController.cs
+++ NGJ2019_Training/Assets/PlayerController.cs
@@ -4,153 +4,166 @@ using UnityEngine;
 
 public class PlayerController : GameControllerCommunicator
 {
-	public ParticleSystem Win;
+    public ParticleSystem Win;
 
-	public GameObject CurrentPiece;
+    public GameObject CurrentPiece;
 
-	public KeyCode Rotate;
-	public KeyCode Left;
-	public KeyCode Right;
-	public KeyCode Down;
-	public KeyCode LeftDash;
-	public KeyCode RightDash;
-	public KeyCode Defense;
-	public KeyCode Offense;
+    public KeyCode Rotate;
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Down;
+    public KeyCode LeftDash;
+    public KeyCode RightDash;
+    public KeyCode Defense;
+    public KeyCode Offense;
 
-	private SpawnerScript Spawner;
-	private GameController Controller;
+    private SpawnerScript Spawner;
+    private GameController Controller;
 
-	private int i;
-	private bool IsFinished = false;
-	public bool BoxInside = false;
-	private int timer;
+    private int i;
+    private bool IsFinished = false;
+    public bool BoxInside = false;
+    private int timer;
 
-	private int movePieceInterval = 10;
+    private int movePieceInterval = 8;
 
-	private float LeftBorder;
-	private float RightBorder;
+    private float LeftBorder;
+    private float RightBorder;
 
-	private bool HasPower;
+    private bool HasPower;
 
-	void Awake()
-	{
-		Controller = GameObject.FindObjectOfType<GameController>();
-		Spawner = GetComponentInChildren<SpawnerScript>();
-		LeftBorder = this.transform.position.x - this.GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-		RightBorder = this.transform.position.x + this.GetComponentInChildren<SpriteRenderer>().bounds.size.x;
-	}
+    public List<TetrominoScript> Pieces = new List<TetrominoScript>();
 
-	public override void OnGameStart()
-	{
-		CurrentPiece = Spawner.SpawnNew(this);
-	}
+    void Awake()
+    {
+        Controller = GameObject.FindObjectOfType<GameController>();
+        Spawner = GetComponentInChildren<SpawnerScript>();
+        LeftBorder = this.transform.position.x - this.GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+        RightBorder = this.transform.position.x + this.GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+    }
 
-	public override void FuckMeUp()
-	{
-		CurrentPiece.transform.localScale *= 2;
-		CurrentPiece.GetComponent<Rigidbody2D>().mass *= 2;
-	}
+    public override void OnGameStart()
+    {
+        CurrentPiece = Spawner.SpawnNew(this);
+    }
 
-	public void PlayerFinished()
-	{
+    public override void FuckMeUp()
+    {
+        CurrentPiece.transform.localScale *= 2;
+        CurrentPiece.GetComponent<Rigidbody2D>().mass *= 2;
+    }
 
-		if (!IsFinished)
-		{         //Spawner.NextPiece = null;
-			Destroy(CurrentPiece);
-			IsFinished = true;
-		}
-	}
+    public void PlayerFinished()
+    {
 
-	// Update is called once per frame
-	void FixedUpdate()
-	{
-		if (HasWon) return;
+        if (!IsFinished)
+        {         //Spawner.NextPiece = null;
+            Destroy(CurrentPiece);
+            IsFinished = true;
+        }
+    }
 
-		if (BoxInside)
-		{
-			timer++;
-		}
-		else if (IsFinished)
-		{
-			timer = 0;
-			CurrentPiece = Spawner.SpawnNew(this);
-			IsFinished = false;
-		}
-		if (timer > 200)
-		{
-			Instantiate(Win, new Vector3(this.transform.position.x, Controller.CurrentHighPoint), Quaternion.identity);
-			HasWon = true;
-		}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (HasWon) return;
 
-		i++;
-		if (CurrentPiece == null)
-		{
-			return;
-		}
+        if (BoxInside)
+        {
+            timer++;
+        }
+        else if (IsFinished)
+        {
+            timer = 0;
+            CurrentPiece = Spawner.SpawnNew(this);
+            IsFinished = false;
+        }
+        if (timer > 200)
+        {
+            Instantiate(Win, new Vector3(this.transform.position.x, Controller.CurrentHighPoint), Quaternion.identity);
+            HasWon = true;
+        }
 
-		Rigidbody2D rb = CurrentPiece.GetComponent<Rigidbody2D>();
+        i++;
+        if (CurrentPiece == null)
+        {
+            return;
+        }
 
-		//rb.position += (Vector2.down * Time.deltaTime) * 4;
+        Rigidbody2D rb = CurrentPiece.GetComponent<Rigidbody2D>();
+        TetrominoScript ts = CurrentPiece.GetComponent<TetrominoScript>();
 
-		if (Input.GetKeyDown(Rotate))
-		{
-			rb.rotation -= 90;
-		}
+        //rb.position += (Vector2.down * Time.deltaTime) * 4;
 
-		if (Input.GetKey(Left) && rb.position.x > LeftBorder)
-		{
-			if (i > movePieceInterval)
-			{
-				rb.position += (Vector2.left / 2);
-				i = 0;
-			}
-		}
+        if (Input.GetKeyDown(Rotate))
+        {
+            if (i > movePieceInterval / 2)
+            {
+                rb.rotation -= 90;
+                ts.ExpectedAngle -= 90;
+                i = 0;
+            }
+        }
 
-		if (Input.GetKey(Right) && rb.position.x < RightBorder)
-		{
-			if (i > movePieceInterval)
-			{
-				rb.position += (Vector2.right / 2);
-				i = 0;
-			}
-		}
+        if (Input.GetKey(Left) && rb.position.x > LeftBorder)
+        {
+            if (i > movePieceInterval)
+            {
+                rb.position += (Vector2.left / 2);
+                i = 0;
+            }
+        }
 
-		if (Input.GetKey(Down))
-		{
-			rb.position += Vector2.down / 10;
-		}
+        if (Input.GetKey(Right) && rb.position.x < RightBorder)
+        {
+            if (i > movePieceInterval)
+            {
+                rb.position += (Vector2.right / 2);
+                i = 0;
+            }
+        }
 
-		if (Input.GetKeyDown(LeftDash) && rb.position.x > LeftBorder)
-		{
-			rb.position += (Vector2.left);
-		}
+        if (Input.GetKey(Down))
+        {
+            rb.position += Vector2.down / 10;
+        }
 
-		if (Input.GetKeyDown(RightDash) && rb.position.x < RightBorder)
-		{
-			rb.position += (Vector2.right);
-		}
+        if (Input.GetKeyDown(LeftDash) && rb.position.x > LeftBorder)
+        {
+            rb.position += (Vector2.left);
+        }
 
-		if (Input.GetKeyDown(Offense))
-		{
-			Controller.UsePower(this.GetComponent<GameControllerCommunicator>());
-		}
+        if (Input.GetKeyDown(RightDash) && rb.position.x < RightBorder)
+        {
+            rb.position += (Vector2.right);
+        }
 
-		if (Input.GetKeyDown(Defense))
-		{
-			Controller.UsePower(this.GetComponent<GameControllerCommunicator>());
-		}
-	}
+        if (Input.GetKeyDown(Offense))
+        {
+            Controller.UsePower(this.GetComponent<GameControllerCommunicator>());
+        }
 
-	public bool HasWon { get; set; }
+        if (Input.GetKeyDown(Defense))
+        {
+            Controller.UsePower(this.GetComponent<GameControllerCommunicator>());
+        }
+    }
 
-	public void TetrominoCollided()
-	{
-		CurrentPiece.GetComponent<Rigidbody2D>().gravityScale = 1f;
-		CurrentPiece = Spawner.SpawnNew(this);
-	}
+    public bool HasWon { get; set; }
 
-	public void TetrominoLost()
-	{
-		// TODO
-	}
+    public void TetrominoCollided()
+    {
+        CurrentPiece.GetComponent<Rigidbody2D>().gravityScale = 1f;
+        foreach (var tetrominoScript in Pieces)
+        {
+            //tetrominoScript.SetPreviousState();
+        }
+        Pieces.Add(CurrentPiece.GetComponent<TetrominoScript>());
+        CurrentPiece = Spawner.SpawnNew(this);
+    }
+
+    public void TetrominoLost()
+    {
+        // TODO
+    }
 }
--- NGJ2019_Training/Assets/Scripts/TetrominoScript.cs
+++ NGJ2019_Training/Assets/Scripts/TetrominoScript.cs
@@ -6,57 +6,45 @@ using UnityEngine;
 
 public enum TetrominoType
 {
-	I = 0,
-	O = 1,
-	T = 2,
-	S = 3,
-	Z = 4,
-	J = 5,
-	L = 6
+    I = 0,
+    O = 1,
+    T = 2,
+    S = 3,
+    Z = 4,
+    J = 5,
+    L = 6
 }
 
 public class TetrominoScript : MonoBehaviour
 {
-	private bool HasCollided;
-	public TetrominoType Type;
-	public PlayerController PlayerController;
+    private bool HasCollided;
+    public TetrominoType Type;
+    public PlayerController PlayerController;
     private Rigidbody2D rigidbody2d;
     private float speed = 5f;//Replace with your max speed
+    private bool Corrected;
+    public float ExpectedAngle;
 
     public float OverlapFactor = 0.01f;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		this.gameObject.layer = LayerMask.NameToLayer("Default");
-	    rigidbody2d = this.gameObject.GetComponent<Rigidbody2D>();
-	}
+    //public Vector3 PrevPosition;
+    //public float PrevRotation;
+    //public Vector2 PrevVelocity;
+    //public float PrevAngularVelocity;
 
-	void Update()
-	{
-		if (transform.position.y < -50)
-		{
-			PlayerController.TetrominoLost();
-			Destroy(this.gameObject);
-		}
-	}
-
-    /*void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (!HasCollided)
-		{
-			PlayerController.TetrominoCollided();
-			this.gameObject.layer = LayerMask.NameToLayer("Tetromino");
-			HasCollided = true;
-		}
-	}*/
-
-    void FixedUpdate()
+    // Start is called before the first frame update
+    void Start()
     {
-        if ((rigidbody2d.velocity.magnitude > speed || rigidbody2d.velocity.magnitude < speed) && !HasCollided)
+        this.gameObject.layer = LayerMask.NameToLayer("Default");
+        rigidbody2d = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -50)
         {
-            //print(rigidbody2d.velocity);
-            rigidbody2d.velocity = rigidbody2d.velocity.normalized * speed;
+            PlayerController.TetrominoLost();
+            Destroy(this.gameObject);
         }
     }
 
@@ -64,17 +52,69 @@ public class TetrominoScript : MonoBehaviour
     {
         if (!HasCollided)
         {
-            Camera.main.GetComponent<CameraController>().ShakeCamera();
-
-            var testCollider = collision.collider.bounds;
-            testCollider.size = new Vector3(testCollider.size.x * 1f, testCollider.size.y * 1, testCollider.size.z * 1);
-            HasCollided = testCollider.Intersects(collision.otherCollider.bounds);
-            //print(testCollider.Intersects(collision.otherCollider.bounds));
-            //print("This: " + collision.collider.bounds);
-            //print("Other: " + collision.otherCollider.bounds);
-            CallCollided();
+            PlayerController.TetrominoCollided();
+            this.gameObject.layer = LayerMask.NameToLayer("Tetromino");
+            HasCollided = true;
         }
     }
+
+    //public void SetPreviousState()
+    //{
+    //    this.transform.position = PrevPosition;
+    //    this.transform.rotation = Quaternion.Euler(0, 0, PrevRotation);
+    //    this.rigidbody2d.velocity = PrevVelocity;
+    //    this.rigidbody2d.angularVelocity = PrevAngularVelocity;
+    //}
+
+    void FixedUpdate()
+    {
+        if ((rigidbody2d.velocity.magnitude > speed || rigidbody2d.velocity.magnitude < speed) && !HasCollided)
+        {
+            rigidbody2d.velocity = rigidbody2d.velocity.normalized * speed;
+        }
+
+        //if (HasCollided && !Corrected)
+        //{
+        //    print(rigidbody2d.velocity);
+
+        //    rigidbody2d.velocity = Vector2.zero;
+        //    rigidbody2d.angularVelocity = 0;
+        //    var height = (int)((transform.position.y + 0.1f) * 2);
+        //    this.transform.position = new Vector3(this.transform.position.x, height / 2f, 0);
+
+        //    var newRotation = Quaternion.Euler(0, 0, ExpectedAngle);
+        //    this.transform.localRotation = newRotation;
+
+        //    //this.transform.rotation = newRotation;
+        //    Corrected = true;
+        //}
+
+        //SaveState();
+    }
+
+    //private void SaveState()
+    //{
+    //    PrevPosition = this.transform.position;
+    //    PrevRotation = this.transform.rotation.eulerAngles.z;
+    //    PrevVelocity = this.rigidbody2d.velocity;
+    //    PrevAngularVelocity = this.rigidbody2d.angularVelocity;
+    //}
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (!HasCollided)
+    //    {
+    //        Camera.main.GetComponent<CameraController>().ShakeCamera();
+
+    //        var testCollider = collision.collider.bounds;
+    //        testCollider.size = new Vector3(testCollider.size.x * 1f, testCollider.size.y * 1, testCollider.size.z * 1);
+    //        HasCollided = testCollider.Intersects(collision.otherCollider.bounds);
+    //        //print(testCollider.Intersects(collision.otherCollider.bounds));
+    //        //print("This: " + collision.collider.bounds);
+    //        //print("Other: " + collision.otherCollider.bounds);
+    //        CallCollided();
+    //    }
+    //}
 
     private bool hasCalledCollided = false;
     private void CallCollided()
@@ -83,8 +123,8 @@ public class TetrominoScript : MonoBehaviour
         {
             //rigidbody2d.mass = 500f;
             rigidbody2d.bodyType = RigidbodyType2D.Dynamic;
-            hasCalledCollided = true;
             PlayerController.TetrominoCollided();
+            hasCalledCollided = true;
             this.gameObject.layer = LayerMask.NameToLayer("Tetromino");
         }
     }
